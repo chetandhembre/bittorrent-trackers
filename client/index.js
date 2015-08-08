fb68d48b56879ba4bc3ff63005999cc0ede3fc82
Chetan <chetandhembre@gmail.com>
2015-08-09 02:46:28 +0530

    initial commit

---
+++ client/index.js
@@ -0,0 +1,58 @@
+var torrentUpload = require('../lib/torrentUpload')
+var enterInput = require('enter-input')
+var handleInput= require('../lib/handleInput')
+var TorrentFile = handleInput.TorrentFile
+var RemoteTorrent = handleInput.RemoteTorrent
+var ipLocation = require('../lib/ipLocation')
+var gmap = require('../lib/gmap')
+
+var fileElement = document.querySelector('#file')
+var magnetLink = document.querySelector('#magnetlink')
+
+function onPeers (peers) {
+  peers.slice(0, 5).forEach(function (peer) {
+    ipLocation(peer, function (err, lat, lng) {
+        if(!err) {
+          mapIt(lat, lng)
+        }
+    })
+  })
+}
+
+function mapIt (lat, lng) {
+  gmap.addToMap(lat, lng)
+}
+
+var remoteTorrent = new RemoteTorrent(document.location.href+'VODO_Haphead_bundle.torrent')
+remoteTorrent.once('error', function() {
+  remoteTorrent.removeListener('peers', onPeers)
+})
+remoteTorrent.on('peers', onPeers)
+remoteTorrent.once('done', function () {
+  remoteTorrent.removeListener('peers', onPeers)
+})
+
+torrentUpload(fileElement, function (err, file) {
+  if (err) {
+    throw err
+  }
+
+  remoteTorrent.removeListener('peers', onPeers)
+  gmap.reset()
+  var torrentFile = new TorrentFile(file)
+  torrentFile.once('error', function() {
+    torrentFile.removeListener('peers', onPeers)
+  })
+  torrentFile.on('peers', onPeers)
+  torrentFile.once('done', function () {
+    torrentFile.removeListener('peers', onPeers)
+  })
+})
+
+
+
+
+
+
+
+
---
+++ server/server.js
@@ -0,0 +1,49 @@
+var Hapi = require('hapi')
+var udpTracker = require('../lib/udpTracker')
+
+var server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: 8000 
+});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'client'
+        }
+    }
+});
+
+server.route({
+	method: 'GET',
+	path: '/api/announce',
+	handler: function (request, reply) {
+		var infoHash = request.query['infoHash']
+		var announce = request.query['announce']
+		var ips = []
+		if (isUdp(announce)) {
+			udpTracker.getTrackers(infoHash, announce, function (err, res) {
+				if (err) {
+					return reply(err)
+				}
+
+				return reply(res)
+			})
+		} else {
+			return reply(ips) 	
+		}
+
+		
+	}
+})
+
+function isUdp (url) {
+	return url.indexOf('udp:') === 0
+}
+
+
+// Start the server
+server.start();
